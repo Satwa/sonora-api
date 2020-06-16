38202796682db80f131451d7d09d4ec152099412
Joshua <satwa.fr@gmail.com>
2020-06-16 23:07:44 +0200

    test route and database link

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    $results = DB::select("SELECT * FROM songs_data");
+    return response(json_encode($results))
+                ->header('Content-Type', 'application/json');
 });
